2fe80107ffa5ec2ed9a3135f88e7157438a1dd56
manuelmenama <manuel.mena090589@gmail.com>
2023-03-17 11:11:13 +0100

    tested date

--- src/org/lessons/java/events/Evento.java
+++ src/org/lessons/java/events/Evento.java
@@ -1,4 +1,64 @@
 package org.lessons.java.events;
 
+import java.time.LocalDate;
+
 public class Evento {
+    //attributi
+    private String titolo;
+    private LocalDate data;
+    private int capienzaMassima;
+    private static int postiPrenotati;
+
+    //costruttori
+
+    public Evento(String titolo, LocalDate data, int capienzaMassima) throws IllegalArgumentException{
+        this.titolo = titolo;
+        if (LocalDate.now().isAfter(data)){
+            throw new IllegalArgumentException("La data deve essere successiva a " + LocalDate.now() + ".");
+        }
+        this.data = data;
+        this.capienzaMassima = capienzaMassima;
+        postiPrenotati = 0;
+    }
+
+    //getter e setter
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public int getCapienzaMassima() {
+        return capienzaMassima;
+    }
+
+    public void setCapienzaMassima(int capienzaMassima) {
+        this.capienzaMassima = capienzaMassima;
+    }
+
+    public static int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    //metodi
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "titolo='" + titolo + '\'' +
+                ", data=" + data +
+                ", capienzaMassima=" + capienzaMassima +
+                '}';
+    }
 }
--- src/org/lessons/java/events/Main.java
+++ src/org/lessons/java/events/Main.java
@@ -1,7 +1,12 @@
 package org.lessons.java.events;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        Evento evento = new Evento("De Bello Gallico", LocalDate.of(2024,1,1), 10000);
+
+        System.out.println(evento.toString());
     }
 }
